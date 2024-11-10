f085e6051b97c1beb3aafa1424e151130c6313bb
Tom D Bebbington <tombebb@protonmail.com>
2024-11-10 20:24:37 +0000

    Test future changes

--- tests/TestFutureBasics.hx
+++ tests/TestFutureBasics.hx
@@ -1,3 +1,5 @@
+import utest.Async;
+import haxe.Timer;
 import utest.Assert;
 import utest.Test;
 import xasync.Future;
@@ -13,5 +15,16 @@ class TestFutureBasics extends Test {
 		var ran = false;
 		new Future({}, (changeState) -> ran = true);
 		Assert.isTrue(ran);
+	}
+
+	@:timeout(200)
+	function testSignalRan(async:Async) {
+		var future = new Future(-1, (stateChange) -> Timer.delay(() -> {
+			stateChange(1);
+			async.done();
+		}, 100));
+		future.stateChanged += curr -> {
+			Assert.pass();
+		};
 	}
 }
