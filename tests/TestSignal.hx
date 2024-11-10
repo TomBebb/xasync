9593ca6ae7d3195c374fd5e09623762fb5b8480b
Tom D Bebbington <tombebb@protonmail.com>
2024-11-10 19:18:59 +0000

    Add multi tests

--- tests/TestSignal.hx
+++ tests/TestSignal.hx
@@ -10,4 +10,27 @@ class TestSignal extends Test {
 		signal.invoke(true);
 		Assert.isTrue(ran);
 	}
+
+	function testMultiBasic() {
+		var totalRuns = 10;
+		var execRuns = 0;
+		var signal = new Signal<Int>();
+		for (i in 0...totalRuns) {
+			signal.addListener((v) -> execRuns++);
+		}
+		signal.invoke(-1);
+		Assert.equals(totalRuns, execRuns);
+	}
+
+	function testMultiWithRemove() {
+		var totalRuns = 10;
+		var execRuns = 0;
+		var signal = new Signal<Int>();
+		for (i in 0...totalRuns) {
+			signal.addListener((v) -> execRuns++);
+		}
+		signal.removeListener(signal.listeners[0]);
+		signal.invoke(-1);
+		Assert.equals(totalRuns - 1, execRuns);
+	}
 }
